69473b852f3021f5c918863e44b3216f6e2b858c
Crister48 <2508912994@qq.com>
2021-07-13 19:05:59 +0800

    学生和管理员个人中心，登陆界面

--- thinkphp5.0guide-step1/application/index/controller/AdminCenterController.php
+++ thinkphp5.0guide-step1/application/index/controller/AdminCenterController.php
@@ -2,10 +2,10 @@
 namespace app\index\controller;
 use think\Controller;
 
-class AdminCenterController extends Controller
+class AdminCenterController extends Controller<--管理员端个人中心-->
 {
-	public function center()
-	{
-		return $this->fetch();
-	}
+    public function center()
+    {
+        return $this->fetch();
+    }//查看个人中心界面
 }
