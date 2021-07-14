c6914e69a85835cd1d9573b35822fb5ee0d18d29
Crister48 <2508912994@qq.com>
2021-07-14 10:25:49 +0800

    登录操作（初期）

--- thinkphp5.0guide-step1/application/index/controller/LogInController.php
+++ thinkphp5.0guide-step1/application/index/controller/LogInController.php
@@ -1,11 +1,38 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
-
+use think\Request; 
+use app\common\model\Teacher;
 class LogInController extends Controller
 {
-	public function index()
-	{
-		return $this->fetch();
-	}
+    public function index()
+    {
+        
+        return $this->fetch();
+    }
+
+    // 处理用户提交的登录数据
+    public function login()
+    {
+        // 接收post信息
+        $postData = Request::instance()->post();
+
+        // 验证用户名是否存在
+        $map = array('username'  => $postData['username']);
+        $User = User::get($map);
+        // $User要么是一个对象，要么是null。
+        if (!is_null($User) && $User->getData('password') === $postData['password']) {
+            // 用户名密码正确，将userId存session，并跳转至教师管理界面
+            session('userId', $User->getData('id'));
+            return $this->success('登录成功', url('/index'));
+        } else {
+            // 用户名不存在，跳转到登录界面。
+            return $this->error('用户名或密码错误', url('index'));
+        }
+    }
+
+// 验证用户名是否存在
+// 验证密码是否正确
+// 用户名密码正确 ，将teacherId 存session
+// 用户名密码错误，跳转到登录界面 
 }
