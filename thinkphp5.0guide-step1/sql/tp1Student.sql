dc093de0b203b6f5f2754e429bdceab72d984572
前端-赵一格 <1746922468@qq.com>
2021-07-15 16:27:51 +0800

    修改学生数据库的属性

--- thinkphp5.0guide-step1/sql/tp1Student.sql
+++ thinkphp5.0guide-step1/sql/tp1Student.sql
@@ -3,15 +3,15 @@
 
  Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 100113
+ Source Server Version : 80022
  Source Host           : localhost:3306
  Source Schema         : tp1
 
  Target Server Type    : MySQL
- Target Server Version : 100113
+ Target Server Version : 80022
  File Encoding         : 65001
 
- Date: 14/07/2021 10:45:50
+ Date: 15/07/2021 16:24:23
 */
 
 SET NAMES utf8mb4;
@@ -22,15 +22,16 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `yunzhi_student`;
 CREATE TABLE `yunzhi_student`  (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int unsigned NOT NULL,
   `name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '姓名',
-  `sex` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '0男，1女',
+  `sex` tinyint unsigned NOT NULL COMMENT '0男，1女',
   `username` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
   `email` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '邮箱',
-  `create_time` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
-  `update_time` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
+  `create_time` int unsigned NOT NULL COMMENT '创建时间',
+  `update_time` int unsigned NOT NULL COMMENT '更新时间',
   `password` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '123' COMMENT '密码',
-  `number` int(10) NULL DEFAULT 0,
+  `number` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0' COMMENT '学号',
+  `klass_id` int(0) NOT NULL COMMENT '所在班级',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
