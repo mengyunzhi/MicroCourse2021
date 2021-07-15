52fac065f200526f340d821d589a279508685d05
前端-赵一格 <1746922468@qq.com>
2021-07-15 14:18:32 +0800

    增加了编辑一条班级的功能

--- thinkphp5.0guide-step1/application/index/controller/KlassController.php
+++ thinkphp5.0guide-step1/application/index/controller/KlassController.php
@@ -37,13 +37,60 @@ class KlassController extends Controller
         return $this->fetch();
     }
 
-    public function save()
+    // public function save()
+    // {
+    //     $Request = Request::instance();
+    //     $Klass = new Klass();
+    //     $Klass->name = $Request->post('name');
+    //     var_dump($Klass->save());
+    //     return $Klass->name . '成功增加至数据表中。新增ID为:' . $Klass->id;
+    // }
+        public function save()
     {
+        // 实例化请求信息
         $Request = Request::instance();
+
+        // 实例化班级并赋值
         $Klass = new Klass();
         $Klass->name = $Request->post('name');
-        var_dump($Klass->save());
-        return $Klass->name . '成功增加至数据表中。新增ID为:' . $Klass->id;
+
+        // 添加数据
+        if (!$Klass->validate(true)->save()) {
+            return $this->error('数据添加错误：' . $Klass->getError());
+        }
+
+        return $this->success('操作成功', url('index'));
+    }
+    public function edit()
+    {
+        $id = Request::instance()->param('id/d');
+
+        // 获取用户操作的班级信息
+        if (false === $Klass = Klass::get($id))
+        {
+            return $this->error('系统未找到ID为' . $id . '的记录');
+        }
+
+        $this->assign('Klass', $Klass);
+        return $this->fetch();
     }
 
+    public function update()
+    {
+        $id = Request::instance()->post('id/d');
+
+        // 获取传入的班级信息
+        $Klass = Klass::get($id);
+        if (is_null($Klass)) {
+            return $this->error('系统未找到ID为' . $id . '的记录');
+        }
+
+        // 数据更新
+        $Klass->name = Request::instance()->post('name');
+        if (!$Klass->validate()->save()) { 
+            return $this->error('更新错误：' . $Klass->getError());
+        } else {
+            return $this->success('操作成功', url('index'));
+        }
+    }
 }
