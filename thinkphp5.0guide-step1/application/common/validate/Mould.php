7a8b421977bea9e43c301efa1674bce6f3b20e55
feng <1589654294@qq.com>
2021-07-16 10:09:58 +0800

    管理员端的模板管理功能完善

--- thinkphp5.0guide-step1/application/common/validate/Mould.php
+++ thinkphp5.0guide-step1/application/common/validate/Mould.php
@@ -5,7 +5,6 @@ use think\Validate;
 class Mould extends Validate
 {
     protected $rule = [
-    	'id' => 'require',
     	'name' => 'require|length:2,25',
     	'row' => 'require',
     	'line' => 'require',
