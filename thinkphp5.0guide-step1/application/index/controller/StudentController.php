e6de893e8aef729c1a0768a014bd46b5c2f5609d
Crister48 <2508912994@qq.com>
2021-07-21 11:29:24 +0800

    a

--- thinkphp5.0guide-step1/application/index/controller/StudentController.php
+++ thinkphp5.0guide-step1/application/index/controller/StudentController.php
@@ -48,6 +48,8 @@ class StudentController extends Index2Controller
             $this->assign('scores',$assignScore);
             $this->assign('Course',new Course);
             return $this->fetch();
+        }
+
 
     public function getCourse($klass_id)
     {
@@ -85,41 +87,8 @@ class StudentController extends Index2Controller
         $score=Score::get($scoreid);
         return $score;
     }
-<<<<<<< HEAD
-    public function index()
-    {
-        $name=Request::instance()->get('name');
-        $id=session('id');
-        $course_id=null;
-        $pageSize = 5; // 每页显示5条数据
-        $Course=new Course;
-        $courses=$Course->select();
-        $Score=new Score;
-        $scores=$Score->select();
-        $assignScore=array();
-        if(!is_null($name)){
-            for ($i=0; $i <count($courses) ; $i++) { 
-                if($courses[$i]->name==$name){
-                    $course_id=$courses[$i]->id;
-                }
-            }
-            for ($i=0; $i <count($scores) ; $i++) { 
-                if($scores[$i]->student_id==$id And $scores[$i]->course_id==$course_id){
-                    $assignScore[0]=$scores[$i];
-                }
-            }
-        }else{
-            $assignScore = $Score->where('student_id',$id )->paginate($pageSize, false, [
-                'query'=>[
-                    'student_id' => $id,
-                    ],
-                ]); 
-        }
-       
-        $this->assign('scores',$assignScore);
-        $this->assign('Course',new Course);
-        return $this->fetch();
-    }
+
+    
     /*
 	public function index()
 	{
@@ -196,7 +165,7 @@ class StudentController extends Index2Controller
 	{
 		return $this->fetch();
 	}
-	
+	*/
 	public function check()
 	{	
         $name = Request::instance()->get('name');
